9d929d5589bbd3bb991f6a085fa21caa3c00f597
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-03 19:15:20 +0300

    Add support for DHT22 sensor

--- src/rpi_sensors.nim
+++ src/rpi_sensors.nim
@@ -1,5 +1,15 @@
+import os
+import nimpy
+
+let Adafruit_DHT = pyImport("Adafruit_DHT")
+
 proc main =
-  echo "rpi_sensors"
+  while true:
+    let device = Adafruit_DHT.DHT22
+    let sensors = Adafruit_DHT.read_retry(device, 4)
+    echo "Temperature: ", sensors[0], ", humidity: ", sensors[1]
+    os.sleep(1000)
+
 
 when isMainModule:
   main()
