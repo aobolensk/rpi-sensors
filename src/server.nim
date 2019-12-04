e83374892ff83ea114d9b13f7c42bfcec595f175
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-04 11:47:21 +0300

    (#1) Display DHT22 sensor information on webpage

--- src/server.nim
+++ src/server.nim
@@ -1,9 +1,15 @@
 import asynchttpserver, asyncdispatch
+import dht22
+
+let sensor = dht22(4)
 
 let server = newAsyncHttpServer()
 
 proc serveRequest(req: Request) {.async.} =
-  await req.respond(Http400, "Hello")
+  {.gcsafe.}:
+    let data = sensor.get()
+  let dht22Responce = "Temperature: " & $(data[0]) & ", humidity: " & $(data[1])
+  await req.respond(Http400, dht22Responce)
 
 proc main =
   waitFor server.serve(Port(8080), serveRequest)
