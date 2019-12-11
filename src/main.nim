03b47b7fbab8d5d4fe4b9d5de9a2132ed08e57dc
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-11 23:29:44 +0300

    Add support for hc_sr501 motion detector sensor

--- src/main.nim
+++ src/main.nim
@@ -1,14 +1,17 @@
 import os
 
 import sensors/dht22
+import sensors/hc_sr501
 
 proc main =
   while true:
-    let sensor = dht22(4)
+    let sensor = hc_sr501(4)
     let data = sensor.get()
-    echo "Temperature: ", data[0], ", humidity: ", data[1]
-    os.sleep(1000)
-
+    if data > 0:
+      echo "Motion is detected"
+    else:
+      echo "Motion is not detected"
+    os.sleep(500)
 
 when isMainModule:
   main()
