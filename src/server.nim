1533aa9c492881f147c8bef16171ed2f37ed9f48
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-14 01:27:07 +0300

    (#9) Handle 404 error
    close #9

--- src/server.nim
+++ src/server.nim
@@ -7,27 +7,30 @@ import sensors/mh_rd
 let server = newAsyncHttpServer()
 
 proc serveRequest(req: Request) {.async.} =
-  var response = ""
-  block dht22:
-    let sensor = dht22(4)
-    {.gcsafe.}:
+  if req.url.path == "/":
+    var response = ""
+    block dht22:
+      let sensor = dht22(4)
+      {.gcsafe.}:
+        let data = sensor.get()
+      response &= "[DHT-22] Temperature: " & $(data[0]) & ", humidity: " & $(data[1]) & '\n'
+    block hc_sr501:
+      let sensor = hc_sr501(17)
       let data = sensor.get()
-    response &= "[DHT-22] Temperature: " & $(data[0]) & ", humidity: " & $(data[1]) & '\n'
-  block hc_sr501:
-    let sensor = hc_sr501(17)
-    let data = sensor.get()
-    if data > 0:
-      response &= "[HC-SR501] Motion is detected\n"
-    else:
-      response &= "[HC-SR501] Motion is not detected\n"
-  block mh_rd:
-    let sensor = mh_rd(24)
-    let data = sensor.get()
-    if data > 0:
-      response &= "[MH-RD] Rain is not detected\n"
-    else:
-      response &= "[MH-RD] Rain is detected\n"
-  await req.respond(Http200, response)
+      if data > 0:
+        response &= "[HC-SR501] Motion is detected\n"
+      else:
+        response &= "[HC-SR501] Motion is not detected\n"
+    block mh_rd:
+      let sensor = mh_rd(24)
+      let data = sensor.get()
+      if data > 0:
+        response &= "[MH-RD] Rain is not detected\n"
+      else:
+        response &= "[MH-RD] Rain is detected\n"
+    await req.respond(Http200, response)
+  else:
+    await req.respond(Http404, "Not found")
 
 proc main =
   waitFor server.serve(Port(8080), serveRequest)
